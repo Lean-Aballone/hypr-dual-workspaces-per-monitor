39ac7342336dcd7ce1c056f3396afe23ba5f2b71
Lean-Aballone <l.aballone@gmail.com>
2024-06-11 21:19:48 -0300

    Removed unused code

--- hyprl_wkchange.cpp
+++ hyprl_wkchange.cpp
@@ -1,49 +1,39 @@
-#include <iostream>
 #include <stdexcept>
-#include <stdio.h>
-#include <string>
 
-using namespace std;
-
-string exec(string command) {
+std::string exec(std::string command) {
    char buffer[128];
-   string result = "";
-
+   std::string result = "";
    // Open pipe to file
    FILE* pipe = popen(command.c_str(), "r");
    if (!pipe) {
       return "popen failed!";
    }
-
    // read till end of process:
    while (!feof(pipe)) {
-
       // use buffer to read and add to result
       if (fgets(buffer, 128, pipe) != NULL)
          result += buffer;
    }
-
    pclose(pipe);
    return result;
 }
-void changeWs(char, string);
+void changeWs(char, std::string);
 int main() {
-   string active_wk = exec("hyprctl activeworkspace | grep -E 'ID' | sed 's/ID//g' |awk '{print $2}'");
+   std::string active_wk = exec("hyprctl activeworkspace | grep -E 'ID' | sed 's/ID//g' |awk '{print $2}'");
    active_wk.pop_back();
    char mon_ID = active_wk.back();
    active_wk.pop_back();
    active_wk.pop_back();
-   string workspace_ID = active_wk;
+   std::string workspace_ID = active_wk;
    changeWs(mon_ID,workspace_ID);
-   return 0;
 }
 
-void changeWs(char mon_ID, string ws_ID){
+void changeWs(char mon_ID, std::string ws_ID){
     switch(mon_ID){
         case '0': //Samsung
             (ws_ID == "4") ? system("hyprctl dispatch workspace 44") : system("hyprctl dispatch workspace 4");
             break;
-        case '1': //philips
+        case '1': //Philips
             (ws_ID == "3") ? system("hyprctl dispatch workspace 33") : system("hyprctl dispatch workspace 3");
             break;
         case '2': //LG
@@ -53,7 +43,7 @@ void changeWs(char mon_ID, string ws_ID){
             (ws_ID == "1") ? system("hyprctl dispatch workspace 11") : system("hyprctl dispatch workspace 1");
             break;
         default:
-            system("hyprctl dispatch workspace 3");
+            system("hyprctl dispatch workspace 3"); //Default (central monitor)
             break;
     }
 }
