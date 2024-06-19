ffa9368460131d0b5e0e94592f89c32d83829533
Lean-Aballone <l.aballone@gmail.com>
2024-06-18 22:46:36 -0300

    Code refactoring

--- wkchange.sh
+++ wkchange.sh
@@ -1,46 +1,17 @@
 #!/bin/bash
 var=($(hyprctl activeworkspace | grep -E 'ID' | sed 's/ID//g' | awk '{print $2}'))
-
-##WORKSPACE
-#${var[0]}
-##MONITOR
-#${var[1]}
+##WORKSPACE #${var[0]}
+##MONITOR #${var[1]}
 
 case ${var[1]} in
-    #samsung
-    0)
-        if [ ${var[0]} == 4 ]; then
-            hyprctl dispatch workspace 44
-        else 
-            hyprctl dispatch workspace 4
-        fi
-        ;;
-    #philips
-    1)
-        if [ ${var[0]} == 3 ]; then
-            hyprctl dispatch workspace 33
-        else 
-            hyprctl dispatch workspace 3
-        fi
-        ;;
-    #LG
-    2)
-        if [ ${var[0]} == '2' ]; then
-            hyprctl dispatch workspace 22
-        else 
-            hyprctl dispatch workspace 2
-        fi
-        ;;
-    #SONY
-    3)
-        if [ ${var[0]} == "1" ]; then
-            hyprctl dispatch workspace 11
-        else 
-            hyprctl dispatch workspace 1
-        fi
-        ;;
-
-    *)
-        hyprctl dispatch workspace 3
-        ;;
+    #samsung(id=0)
+    0) [ ${var[0]} == '4' ] && hyprctl dispatch workspace 44 || hyprctl dispatch workspace 4 ;;
+    #philips(id 1)
+    1) [ ${var[0]} == '3' ] && hyprctl dispatch workspace 33 || hyprctl dispatch workspace 3 ;;
+    #LG(id=2)
+    2) [ ${var[0]} == '2' ] && hyprctl dispatch workspace 22 || hyprctl dispatch workspace 2 ;;
+    #SONY(id=3)
+    3) [ ${var[0]} == '1' ] && hyprctl dispatch workspace 11 || hyprctl dispatch workspace 1 ;;
+    #ERROR/DEFAULT
+    *) hyprctl dispatch workspace 3 ;;
 esac
